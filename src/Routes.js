206e56dc4c0a1521bc5c576019875b9ad8df0882
Suraj Verma <surajverma@rentalcars.com>
2020-09-18 10:28:18 +0100

    wip

--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { Layout } from "antd";
 
-import ComponentDidMount1 from "./componentDidMount/01";
-import ComponentDidMount2 from "./componentDidMount/02";
-
 import Search1 from "./useState/01";
 import Search2 from "./useState/02";
 import Search3 from "./useState/03";
@@ -24,15 +21,15 @@ const Routes = () => {
       <Content className="app">
         <Route
           exact
-          path="/componentDidMount-1"
-          component={ComponentDidMount1}
+          path="/"
+          component={() => {
+            return (
+              <div>
+                <h1 className="text-white">React Marathon</h1>
+              </div>
+            );
+          }}
         />
-        <Route
-          exact
-          path="/componentDidMount-2"
-          component={ComponentDidMount2}
-        />
-
         <Route exact path="/useState-1" component={Search1} />
         <Route exact path="/useState-2" component={Search2} />
         <Route exact path="/useState-3" component={Search3} />
